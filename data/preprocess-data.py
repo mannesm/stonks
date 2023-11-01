c5cb15603d847776febc2dae3877134d470dea5c
mannes <mannes@bunq.com>
2023-11-01 17:20:23 +0100

    Preprocess data

--- data/preprocess-data.py
+++ data/preprocess-data.py
@@ -7,7 +7,7 @@ from constants import START_DATE, END_DATE, TICKER_SYMBOLS
 def add_basic_features():
     df = fetch_data(START_DATE, END_DATE, TICKER_SYMBOLS)
     df['daily_return'] = df.groupby('ticker')['Close'].pct_change()
-    df['cumulative_return'] = (1 + ((df.groupby('ticker')['daily_return'])).cumprod() - 1)  # DOesnt work
+    df['cumulative_return'] = (1 + (df.groupby('ticker')['daily_return']).cumprod() - 1)  # DOesnt work
     df['50_day_moving_avg'] = df.groupby('ticker')['Close'].rolling(window=50).mean().reset_index(level=0, drop=True)
     df['200_day_moving_avg'] = df.groupby('ticker')['Close'].rolling(window=200).mean().reset_index(level=0, drop=True)
     df.sort_index(inplace=True)
